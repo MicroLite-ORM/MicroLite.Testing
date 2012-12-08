4952d93cf0cf97fa5b2c99feb142ddbd6490c059
Trevor Pilley <trev@trevorpilley.co.uk>
2012-12-08 00:12:05 +0000

    Added build output for .NET 4.5

--- default.ps1
+++ default.ps1
@@ -4,9 +4,10 @@ properties {
   $buildDir = "$baseDir\build"
   $buildDir35 = "$buildDir\3.5\"
   $buildDir40 = "$buildDir\4.0\"
+  $buildDir45 = "$buildDir\4.5\"
 }
 
-Task Default -depends Build, Build35, Build40
+Task Default -depends Build, Build35, Build40, Build45
 
 Task Build35 {
   Remove-Item -force -recurse $buildDir35 -ErrorAction SilentlyContinue
@@ -19,7 +20,14 @@ Task Build40 {
   Remove-Item -force -recurse $buildDir40 -ErrorAction SilentlyContinue
   
   Write-Host "Building $projectName.csproj for .net 4.0" -ForegroundColor Green
-  Exec { msbuild "$projectName\$projectName.csproj" /target:Rebuild "/property:Configuration=Release;OutDir=$buildDir40;TargetFrameworkVersion=v4.0;TargetFrameworkProfile=Client" /verbosity:quiet }
+  Exec { msbuild "$projectName\$projectName.csproj" /target:Rebuild "/property:Configuration=Release;DefineConstants=NET_4_0;OutDir=$buildDir40;TargetFrameworkVersion=v4.0;TargetFrameworkProfile=Client" /verbosity:quiet }
+}
+
+Task Build45 {
+  Remove-Item -force -recurse $buildDir45 -ErrorAction SilentlyContinue
+  
+  Write-Host "Building $projectName.csproj for .net 4.5" -ForegroundColor Green
+  Exec { msbuild "$projectName\$projectName.csproj" /target:Rebuild "/property:Configuration=Release;DefineConstants=NET_4_5;OutDir=$buildDir45;TargetFrameworkVersion=v4.0" /verbosity:quiet }
 }
 
 Task Build -Depends Clean {
@@ -28,7 +36,6 @@ Task Build -Depends Clean {
 }
 
 Task Clean {
-
   Write-Host "Cleaning $projectName.sln" -ForegroundColor Green
   Exec { msbuild "$projectName.sln" /t:Clean /p:Configuration=Release /v:quiet }
 }
