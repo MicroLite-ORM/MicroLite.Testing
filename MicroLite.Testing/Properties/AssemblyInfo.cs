2255b13b0ec728967502a6aae6a3b9f9a03e90e0
Trevor Pilley <trev@trevorpilley.co.uk>
2013-01-06 12:46:12 +0000

    Built version 2.0.0

--- MicroLite.Testing/Properties/AssemblyInfo.cs
+++ MicroLite.Testing/Properties/AssemblyInfo.cs
@@ -41,7 +41,7 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
